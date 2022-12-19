8b45d2d8e9046f52be041ce6d5db6a86aa39b9c9
RafaEscobar <rafael.eg.code@gmail.com>
2022-12-19 16:33:12 -0600

    Avance, Estructura del Stack generada

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
-import { NavigationContainer } from '@react-navigation/native';
-import React from 'react'
+import React from 'react';
+import 'react-native-gesture-handler';
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './src/navigator/StackNavigator';
 
 export const App = () => {
   return (
-    <SafeAreaView>
       <NavigationContainer>
-        <Text>Holaaa</Text>
+        <StackNavigator />
       </NavigationContainer>
-    </SafeAreaView>
   );
 };
  
