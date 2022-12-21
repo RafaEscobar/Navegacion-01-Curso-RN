a4e838614a6c5e8f3c80303c15ee1ba6d46f4e9d
RafaEscobar <rafael.eg.code@gmail.com>
2022-12-21 12:46:22 -0600

    Avance, Doumentacion de todo lo realizado, Concluida

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/navigator/StackNavigator';
 
 export const App = () => {
   return (
+    //! 1) Cuando usamos cualquier tipo de navegacion debemos envolver el todo en un <NavigationContainer>
       <NavigationContainer>
+        {/* //! 2) Llamamos a nuestro componente principal >>> En este caso al -StackNavigator- */}
         <StackNavigator />
       </NavigationContainer>
   );
--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -1,21 +1,24 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-
+    // Margen en todas direcciones para todas las Screens
     marginGlobal: {
         margin: 15,
     },
+    // Titulos de todas las Screens
     titleGlobal: {
         fontSize: 25,
         color: 'black',
         textAlign: 'center',
         marginBottom: 15,
     },
+    // Contenedor Horizontal de los btns en las Screens
     btn_contentH: {
         flexDirection: 'row', 
         width: '100%', 
         justifyContent: 'center',
     },
+    // Estilizacion de todos los btns
     btn: {
         width: 100,
         height: 100,
@@ -25,6 +28,7 @@ export const styles = StyleSheet.create({
         marginRight: 15,
         backgroundColor: 'gray',
     },
+    // Estilizacion de todos los textos de los btns
     btn_text: {
         fontSize:12,
         color: 'white',
